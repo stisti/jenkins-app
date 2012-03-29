48dd3ae8a03c415d5c5fc4775cd22484e2554b18
Sami Tikka <sjtikka@gmail.com>
2012-03-29 21:49:46 +0300

    Fix helper script logging

--- Jenkins.app/Contents/Resources/wait_for_jenkins.sh
+++ Jenkins.app/Contents/Resources/wait_for_jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 timeout=$(($(date +%s) + 60))
 while [ $(date +%s) -lt $timeout ] && ! curl -s "$1" >/dev/null; do
-    logger "Jenkins is not responding yet, sleeping..."
+    logger -t Jenkins.app "Jenkins is not responding yet, sleeping..."
     sleep 1
 done
-logger "Not waiting any longer"
+logger -t Jenkins.app "Not waiting any longer"
 curl -s "$1" >/dev/null
